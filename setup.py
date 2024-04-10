f50b97675f9eca8a3e24d41588c5bc2e89f49f05
Ritik Sahni <ritiksahni0203@gmail.com>
2024-04-11 01:23:41 +0530

    Change version in setup.py to 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='chai-cli',
-    version='0.1',
+    version='0.2',
     author='Ritik Sahni',
     author_email='ritiksahni0203@gmail.com',
     description='AI in your CLI.',
