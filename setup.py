3523c3795660a058b25d66787d02e235040af2e7
Ritik Sahni <ritiksahni0203@gmail.com>
2024-04-11 01:35:35 +0530

    Update version to 0.21 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='chai-cli',
-    version='0.2',
+    version='0.2.1',
     author='Ritik Sahni',
     author_email='ritiksahni0203@gmail.com',
     description='AI in your CLI.',
